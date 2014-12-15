44a5e44265adff056fa854558c8850473eeb4db0
Aaron Bergunder <aaronbergunder@gmail.com>
2014-12-15 13:00:57 -0800

    up to full functionality of last running iteration. working on the resetting function now.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,23 @@
 module UsersHelper
 
-	
+	@@last_sunday
 
+	def resetUsers
+		User.find_each do |u|
+  		u.comment = ""
+  		u.attending = 0
+  		u.save
+  	end
+
+  	def dateCheck
+		today = Time.now
+		next_sunday = Time.now.end_of_week
+	
+		if today > @@last_sunday
+			@@last_sunday = next_sunday
+			resetUsers
+		end
+	end
+
+end
 end
