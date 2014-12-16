5ee356487fdeaf26a7b4b8f95e3e04562c668bf5
Aaron Bergunder <aaronbergunder@gmail.com>
2014-12-15 16:56:42 -0800

    unsuccessful attempts at getting the reset to work so far. about to try adding a new model for storing dates.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+	@@last_sunday
+
+	def resetUsers
+		User.find_each do |u|
+  		u.comment = ""
+  		u.attending = 0
+  		u.save
+  	end
+
+  	def dateCheck
+		today = Time.now
+		next_sunday = Time.now.end_of_week
+	
+		puts today
+		puts next_sunday
+		puts @@last_sunday
+
+		if today > @@last_sunday
+			@@last_sunday = next_sunday
+			resetUsers
+		end
+	end
+
+	helper_method :dateCheck
+	helper_method :resetUsers
+
+end
+
 end
